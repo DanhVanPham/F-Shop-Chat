7860dd46939f83820321e173bfe694dcae40bdf3
BinhTT1312 <binhtt.13122000@gmail.com>
2021-03-05 04:26:06 +0700

    rename forml login

--- f-shop-chat/src/containers/Login/index.js
+++ f-shop-chat/src/containers/Login/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import FormLogin from '../../components/Login/index'
+import FormLogin from '../../components/LoginForm/index'
 import '../Login/style.css';
-
+import Banner from '../../assets/banner.svg'
 function Login(props) {
     return (
         <div className="form-container">
             <div className="form-content-left">
-                <img src="img/img-2.svg" alt="spaceship" className="form-img" />
+                <img src={Banner} alt="spaceship" className="form-img" />
             </div>
             <FormLogin />
         </div>
