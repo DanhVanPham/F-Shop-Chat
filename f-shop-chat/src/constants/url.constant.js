3889eb719e84d168356848fad5c42ae8e5483f54
BinhTT1312 <binhtt.13122000@gmail.com>
2021-04-02 13:19:38 +0700

    change root for websocket

--- f-shop-chat/src/constants/url.constant.js
+++ f-shop-chat/src/constants/url.constant.js
@@ -1,2 +1,2 @@
 export const BASE_URL_RESOURCE_SERVER = `http://localhost:8082/v1/api`;
-export const BASE_URL_WEBSOCKET_SERVER = "http://localhost:9090";
+export const BASE_URL_WEBSOCKET_SERVER = "https://app-chat-backend.herokuapp.com/";
